153effff478f6dcbcc0c5f1cbbdebbeef79b44e9
paukerBruce <brucepauker2@gmail.com>
2016-07-12 20:23:58 +0200

    Population : ok

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -47,5 +47,24 @@ class DatabaseSeeder extends Seeder
 				'user_id' => $i+1,
 			]);
 		}
+
+		DB::table('professionals')->delete();
+
+		for($i = 5; $i < 10; ++$i)
+		{
+			DB::table('professionals')->insert([
+				'company_name' => 'Nom'.$i,
+				'category' => 'hotel',
+				'first_name' => 'Jean',
+				'last_name' => 'Dupont',
+				'phone' => '0290942',
+				'zipcode' => 75007,
+				'state' => 'Ile-De-France',
+				'country' => 'France',
+				'address' => '31 avenue Victor Hugo',
+				'credit' => 100,
+				'user_id' => $i+1,
+			]);
+		}
     }
 }
