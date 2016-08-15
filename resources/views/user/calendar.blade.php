f5482638b8265b0d301502b37afbba5314b03662
paukerBruce <brucepauker2@gmail.com>
2016-08-15 00:21:36 -0500

    Mise en place de la vue calendrier

---
+++ resources/views/user/calendar.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master', ["title" => trans('profile.title.student', ["name" => $user->username]), "footer" => false])
+@section('content')
+
+   <div class="row collapse profile profile-container">
+      @include('user.sidebar')
+
+      <div class="medium-10 small-12 columns panel-main">
+
+         <div class="row">
+            <span class="profile-date"><a href="{{ route('calendar', Auth::user()->id) }}">{{ strtoupper(date('h:i A D j M Y')) }}</a></span>
+         </div>
+
+         <div id='calendar'>
+            {!! $calendar->calendar() !!}
+            {!! $calendar->script() !!}
+         </div>
+      </div>
+   </div>
+
+@endsection
