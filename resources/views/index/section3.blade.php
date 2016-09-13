6ee22af11cd9a167996a1cf26f8dcc1ecd968ed6
Bruce Pauker <brucepauker2@gmail.com>
2016-09-13 02:49:31 -0500

    no message

--- resources/views/index/section3.blade.php
+++ resources/views/index/section3.blade.php
@@ -3,7 +3,7 @@
 
    <div class="row">
       <div class="large-4 medium-6 small-12 columns">
-         <h2 class="section-title" style="color: black;">@lang('index.section3.content')</h2>
+         <h2 class="section-title" style="color: black; margin-top:100px;">@lang('index.section3.content')</h2>
       </div>
    </div>
 
