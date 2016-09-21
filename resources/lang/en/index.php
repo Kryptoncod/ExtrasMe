a782b49c71e31b12450f0e0aa129543e2987c3fc
Bruce Pauker <brucepauker2@gmail.com>
2016-09-21 03:36:07 -0500

    no message

--- resources/lang/en/index.php
+++ resources/lang/en/index.php
@@ -6,11 +6,11 @@ return [
    'iAmStudent' => 'I\'m a student',
    'notDeploy' => 'Inscriptions on the Extras Me plateform will be soon available, please contact us +41 79 732 16 09 or at contact@extrasme.com if you should have any inquieries.',
    'section1'   => [
-      'title'     => 'GETTING TOGETHER STUDENTS FROM HOSPITALITY SCHOOLS AROUND THE WORLD WITH PROFESSIONALS FROM THE HOSPITALITY INDUSTRY IN ORDER FOR STUDENTS TO ACCOMPLISH TEMPORARY MISSIONS',
+      'title'     => 'BRINGING TOGETHER STUDENTS AND PROFESSIONALS OF THE HOSPITALITY INDUSTRY WITH SHORT-TERM MISSIONS',
       'signUp' => 'SIGN UP NOW',
    ],
    'section2'   => [
-      'title'     => 'EXCELLENCE OF SERVICE IS PRIMORDIAL',
+      'title'     => 'WE VALUE EXCELLENCE OF SERVICE',
       'moreInfos' => 'MORE INFOS',
    ],
    'section3'   => [
@@ -23,11 +23,11 @@ return [
 
    'missionStatement' => [
       'title' => 'COMPANY MISSION STATEMENT',
-      'content' => 'Extras Me brings renewal to the world of extra work by directly connecting prestigious hotel schools with companies in the hospitality industry. Its purpose is simple: establish a durable connection between a motivated student and any establishment in terms of temporary missions.
+      'content' => 'Extras Me brings renewal to the world of extra work by connecting prestigious hotel schools with companies in the hospitality industry. Its purpose is simple: establish a durable connection between motivated students and establishments for short-term missions.
          <br/><br/>
-         The Extras Me’s offer is dedicated to hospitality professionals but it also represents an important step towards facilitating access to short time employment for hospitality school students. Indeed, our offer is directed towards hospitality professionals to foresee contingency in terms of staff by offering a more qualitative and efficient service. Students will get the opportunity to have access to more extra offers in renowned institutions, hence giving them the opportunity to increase their network and professional experience.
+         The Extras Me’s offer is dedicated to hospitality professionals but it also represents an important step towards facilitating access to short time employment for hospitality school students. Indeed, our offer is directed towards hospitality professionals to foresee contingency in terms of staff by offering a more qualitative and efficient service. Students will get the opportunity to have access extra offers in renowned institutions, giving them the opportunity to increase their network and start a professional experience.
          <br/><br/>
-         The Extras Me team is aware that an excellent service is paramount in all institutions. This is the reason why we differ from other companies approaching our industry, by creating a unique network focused on students from specialized schools. Thus, we are able to provide a qualitative service to all institutions working with us. Individuals and private entities can also use our service for receptions and events, requests that will be handled upon demand.',
+         The Extras Me team is aware that an excellent service is paramount. We differ from other companies because we create a unique network focused on students from specialized schools. Individuals and Private entities can also use our service for receptions and events, requests that will be handled upon demand.',
    ],
 
    'contactUs' => [
--- resources/lang/fr/index.php
+++ resources/lang/fr/index.php
@@ -6,32 +6,29 @@ return [
    'iAmStudent' => 'Je suis un etudiant',
    'notDeploy' => 'Les inscriptions sur la plateforme d’Extras Me seront bientôt accessibles, contactez nous au +41 79 732 16 09 ou à contact@extrasme.com pour plus de renseignements.',
    'section1'   => [
-      'title'     => 'NOUS METTONS EN RELATION DES ÉTUDIANTS D\'ÉCOLES HOTELIÈRES AVEC LES PROFESSIONNELS DU MONDE DE L\'ACCUEIL POUR QUE LES ÉTUDIANTS PUISSENT RÉPONDRE À DES MISSIONS TEMPORAIRES',
+      'title'     => 'NOUS METTONS EN RELATION LES ÉTUDIANTS D\'ÉCOLES HOTELIÈRES AVEC LES PROFESSIONNELS DU MONDE DE L\'ACCUEIL AFIN QUE LES ÉTUDIANTS PUISSENT RÉPONDRE À DES MISSIONS TEMPORAIRES',
       'signUp' => 'S\'INSCRIRE' ,
    ],
    'section2'   => [
-      'title'     => 'PARCE QUE UN TRAVAIL BIEN FAIT NE PEUT ÊTRE QU\'APPRÉCIÉ',
+      'title'     => 'UN PERSONNEL ENGAGÉ POUR UN SERVICE D’EXCELLENCE',
       'moreInfos' => 'EN SAVOIR PLUS',
    ],
    'section3'   => [
       'content' => 'L\'APPLICATION EXTRAS ME SERA BIENTOT DISPONIBLE',
    ],
    'section4'   => [
-      'toEachItsTown'   => 'A CHACUN SA VILLE',
-      'toEachItsExtras' => 'A CHACUN SON EXTRAS',
+      'toEachItsTown'   => 'À CHACUN SA VILLE,',
+      'toEachItsExtras' => 'À CHACUN SON EXTRA',
    ],
 
    'missionStatement' => [
       'title' => 'Mission de l\'Entreprise',
-      'content' => 'Extras Me apporte du renouveau au monde de l’extra en mettant directement en relation les étudiants d’écoles hôtelières prestigieuses avec les entreprises de l’industrie de l’accueil.
-Son but est simple, établir une connexion durable entre un étudiant motivé et un professionnel de l’accueil.
-Notre base de donnée peut également être utilisée par des particuliers pour des réceptions ou des évènements privés.
+      'content' => 'Extras Me apporte du renouveau au monde du travail occasionnel en mettant directement en relation les étudiants d’écoles hôtelières prestigieuses avec les entreprises de l’industrie de l’accueil. Son but est simple, établir une connexion durable entre un étudiant motivé et un professionnel en terme de missions temporaires rémunérées.
          <br/><br/>
-         L’offre d’Extras Me est destinée aux professionnels de l’accueil et représente une avancée importante dans la facilitation de l’accès à l’emploi ponctuel des étudiants en écoles hôtelières spécialisées.
-En effet, notre offre permet aux professionnels de l’accueil de bien s’armer face aux aléas dans la restauration en terme de personnel et d’équipe en proposant un service qualitatif et efficace. Les étudiants auront quant à eux la possibilité d’avoir un accès plus facile qu’auparavant à des offres d’extras dans des établissements de renoms. 
+         L’offre d’Extras Me est destinée aux professionnels de l’accueil et représente une avancée importante dans la facilitation de l’accès à l’emploi ponctuel des étudiants en écoles hôtelières ou écoles spécialisées dans les métiers de l’hôtellerie. En effet, notre offre permet aux professionnels de l’accueil d’éviter les aléas du service en terme de personnel. Nous proposons ainsi une mise en relation plus efficace et qualitative qu’auparavant. Les étudiants auront quant à eux la possibilité d’avoir accès à davantage d’offres d’extras dans des établissements de renoms, ce qui leur permettra d’élargir leur réseau et leur expérience professionnelle.
          <br/><br/>
-         L’équipe d’Extras Me part du principe que l’excellence du service est primordiale dans tous les établissements et c’est ainsi que nous nous différencions des entreprises s’approchant de notre secteur en limitant le réseau aux étudiants d’écoles hôtelières. 
-Ainsi, nous sommes en mesure de garantir un service de qualité à tous les établissements travaillant avec nous.',
+         L’équipe d’Extras Me part du principe que l’excellence du service est essentielle dans tous les établissements. C’est la raison pour laquelle nous nous différencions des entreprises s’approchant de notre secteur, en créant un réseau se limitant aux étudiants d’écoles spécialisées. Ainsi, nous sommes en mesure de garantir un service de qualité à tous les établissements travaillant avec nous. 
+Les particuliers et entreprises peuvent également utiliser nos services pour des réceptions ou événements privés, demandes qui seront traitées individuellement.',
    ],
 
 ];
