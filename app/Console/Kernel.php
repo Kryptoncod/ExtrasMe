5d73396a6707bf6df198b56a8f2926a449b5719d
Bruce Pauker <brucepauker2@gmail.com>
2016-11-12 06:33:44 -0600

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('extra:update')
-                  ->everyMinute();
+         //$schedule->command('extra:update')
+              //    ->everyMinute();
     }
 }
